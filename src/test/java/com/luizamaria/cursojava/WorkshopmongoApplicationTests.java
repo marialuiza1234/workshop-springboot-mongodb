9a6197e812c66e4c6c712a70510df8ea6eb29881
Maria Luiza <luiza.mallu1@gmail.com>
2022-11-24 21:12:38 -0300

    Primeiro commit - projeto criado

---
+++ src/test/java/com/luizamaria/cursojava/WorkshopmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizamaria.cursojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
