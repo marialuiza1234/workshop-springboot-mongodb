2df9f731f4c57986e6582d29bc390e4e0721bd02
Maria Luiza <luiza.mallu1@gmail.com>
2022-11-26 17:11:58 -0300

    Consulta simples com query methods

--- src/main/java/com/luizamaria/cursojava/repository/PostRepository.java
+++ src/main/java/com/luizamaria/cursojava/repository/PostRepository.java
@@ -6,9 +6,9 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 import com.luizamaria.cursojava.domain.Post;
-import com.luizamaria.cursojava.domain.User;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	
 	List<Post> findByTitleContainingIgnoreCase(String text);
 }
