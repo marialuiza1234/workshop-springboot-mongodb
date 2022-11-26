a93dc82004ac13ab593bd731af8a7bbedec67c89
Maria Luiza <luiza.mallu1@gmail.com>
2022-11-26 17:06:22 -0300

    Acrescentando comentários aos posts

--- src/main/java/com/luizamaria/cursojava/repository/PostRepository.java
+++ src/main/java/com/luizamaria/cursojava/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.luizamaria.cursojava.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.luizamaria.cursojava.domain.User;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
