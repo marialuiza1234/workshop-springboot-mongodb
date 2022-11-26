e9a780371004f832f1eaa23ca616835b3db2081d
Maria Luiza <luiza.mallu1@gmail.com>
2022-11-26 16:35:58 -0300

    Endpoint para retornar os posts de um usuário

--- src/main/java/com/luizamaria/cursojava/resources/UserResource.java
+++ src/main/java/com/luizamaria/cursojava/resources/UserResource.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.luizamaria.cursojava.domain.Post;
 import com.luizamaria.cursojava.domain.User;
 import com.luizamaria.cursojava.dto.UserDTO;
 import com.luizamaria.cursojava.services.UserService;
@@ -57,5 +58,11 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+ 	public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 }
